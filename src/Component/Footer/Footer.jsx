f210482198d3ecbc3ee19512530f3503869ba39c
jakiadp <jakiadp@gmail.com>
2025-06-22 23:52:18 +0600

    practice

---
+++ src/Component/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p>
+                <small>Thank you for visiting our website</small>
+                <a href="/">Tearms</a>
+                <a href="/">priavacy</a>
+                <a href="/fb">Facebook</a>
+            </p>
+            
+        </div>
+    );
+};
+
+export default Footer;
